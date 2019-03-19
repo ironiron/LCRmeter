66866e53b65dcb378c635565334e4ced158066fd
Rafał Mazurkiewicz <raf.mazurkiewicz@op.pl>
2019-03-19 21:41:23 +0100

    added docs and tests folders, created test configuration added Pwm class

---
+++ LCRmeter_CODE/Src/Pwm.cpp
@@ -0,0 +1,20 @@
+/*
+ * Pwm.cpp
+ *
+ *  Created on: 19.03.2019
+ *      Author: Rafa³
+ */
+
+#include <Pwm.h>
+
+Pwm::Pwm ()
+{
+  // TODO Auto-generated constructor stub
+
+}
+
+Pwm::~Pwm ()
+{
+  // TODO Auto-generated destructor stub
+}
+
---
+++ LCRmeter_CODE/tests/Pwm_test.cpp
@@ -0,0 +1,9 @@
+/*
+ * Pwm_test.cpp
+ *
+ *  Created on: 19.03.2019
+ *      Author: Rafa³
+ */
+
+#include <Pwm.h>
+
